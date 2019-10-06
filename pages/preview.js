5a71b4f76e7f2c4c13253c80e893e32ea3f2b364
josephmasongsong <joseph@josephmasongsong.com>
2019-10-06 16:41:29 -0700

    errors in preview.js

--- pages/preview.js
+++ pages/preview.js
@@ -1,14 +1,16 @@
 
 import React from 'react'
 import Prismic from 'prismic-javascript'
-import { apiEndpoint, linkResolver } from 'prismic-configuration'
+import { linkResolver } from '../helpers'
+
+const endPoint = process.env.PRISMIC
 
 export default class Preview extends React.Component {
   static async getInitialProps (context) {
     const token = context.query.token
     const { req, res } = context
 
-    const API = await Prismic.getApi(apiEndpoint, { req })
+    const API = await Prismic.getApi(endPoint, { req })
     const url = await API.previewSession(token, linkResolver, '/')
 
     res.writeHead(302, { Location: url })
