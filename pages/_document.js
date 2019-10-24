dda0db4c2a5ef7ba5f71d7ca052d7aa3042464bc
josephmasongsong <joseph@josephmasongsong.com>
2019-10-23 21:23:47 -0700

    moved ga to _app from _document and added purgecss to postcss config

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Router from "next/router";
-import withGA from "next-ga";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -21,4 +19,4 @@ class MyDocument extends Document {
   }
 }
 
-export default withGA("UA-150837765-1", Router)(MyDocument)
+export default MyDocument
--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,20 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  content: [
+    './pages/*.js',
+    './styles/*.css',
+    './components/*.js',
+  ],
+
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
-  plugins: [require('tailwindcss'), require('postcss-preset-env')],
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
 }
