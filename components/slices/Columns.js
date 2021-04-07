05716eaa7740ef31174a8ff059a1ad70086a3117
josephmasongsong <joseph@josephmasongsong.com>
2021-04-06 21:25:51 -0700

    fix small typo

--- components/slices/Columns.js
+++ components/slices/Columns.js
@@ -6,7 +6,7 @@ const Columns = ({ slice }) =>
       <div className="w-full xl:-mx-6 md:flex md:flex-wrap">
         {slice.items && slice.items.map((item, i) => (
           <div className="md:w-1/2 lg:w-1/3 text-center px-6 mt-8 mb-8" key={i}>
-            <span dangerouslySetInnerHTML={{ __html: item.icon }} class="text-5xl" />
+            <span dangerouslySetInnerHTML={{ __html: item.icon }} className="text-5xl" />
             <h4 className="mb-4">
               {RichText.asText(item.header)}
             </h4>
